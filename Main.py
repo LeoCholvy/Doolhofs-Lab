c6930e69040e6b8111255163183b6604b626366f
LeoCholvy <62641325+LeoCholvy@users.noreply.github.com>
2023-05-22 09:02:39 +0200

    Changement taille lab config

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from src.maze_generator import Maze
 from src.Interface import *
 
 config = Get_config()
-maze = Maze(config["hauteur"], config["largeur"])
+maze = Maze(config["taille"], config["taille"])
 level = maze.Get_fmaze()
 lab = Lab(level, config)
 camera_group = CameraGroup(lab)
