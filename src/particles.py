3e1e3eae439ab246f8125151bd04b1783f1b192a
ElBouletos <114732438+ElBouletos@users.noreply.github.com>
2023-05-24 16:44:46 +0200

    Add files via upload

--- src/particles.py
+++ src/particles.py
@@ -1,1 +1,34 @@
 import pygame, random
+
+
+class Particle:
+	def __init__(self, pos):
+		self.x, self.y = pos[0], pos[1]
+		self.vx, self.vy = random.randint(-2,2), random.randint(-2,2)
+		self.rad = 5
+
+	def draw(self, win):
+		pygame.draw.circle(win, (0,0,0), (self.x, self.y), self.rad)
+
+	def update(self):
+		self.x += self.vx
+		self.y += self.vy
+		if random.randint(0, 100) < 40:
+			self.rad -= 1
+
+class Dust:
+
+	def __init__(self, pos):
+		self.pos = pos
+		self.particles = []
+		for i in range(100):
+			self.particles.append(Particle(self.pos))
+
+	def update(self):
+		for i in self.particles:
+			i.update()
+			self.particles = [particle for particle in self.particles if particle.rad > 0]
+
+	def draw(self, win):
+		for i in self.particles:
+			i.draw(win)
